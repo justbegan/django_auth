623a8908bed4fde7de5796cb88490bea409d05f7
justbegan <justbegan@mail.ru>
2024-02-12 11:03:12 +0900

    docker update

--- main_app/settings.py
+++ main_app/settings.py
@@ -1,18 +1,15 @@
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ci0z_eaq*aajt8!mb#%xtb5*ns65&22(jkx6*u^+9w+^h+&pe'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
