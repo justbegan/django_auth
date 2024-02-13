bd95921bf10fce47a802080748bfcd760ee70cf6
justbegan <justbegan@mail.ru>
2024-02-13 14:30:37 +0900

    fix customrefreshtoken

--- apps/jwt_auth/views.py
+++ apps/jwt_auth/views.py
@@ -1,25 +1,25 @@
-from rest_framework_simplejwt.views import TokenViewBase, TokenObtainPairView
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework_simplejwt.exceptions import TokenError
 from rest_framework.views import Response, status
 
 
-class CustomTokenRefresh(TokenViewBase):
-    def post(self, request):
+class CustomTokenRefresh(TokenRefreshView):
+    def get(self, request):
+        cookie_refresh_token = request.COOKIES.get('refresh_token11', None)
+        if not cookie_refresh_token:
+            raise TokenError('No refresh token cookie found')
+
+        refresh_token = {
+            "refresh": cookie_refresh_token
+        }
+        serializer = self.get_serializer(data=refresh_token)
+
         try:
-            # Извлечение refresh токена из куки
-            refresh_token = request.COOKIES.get('refresh_token')
-            if not refresh_token:
-                raise TokenError('No refresh token cookie found')
+            serializer.is_valid(raise_exception=True)
+        except Exception as e:
+            return Response({str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-            # Обновление токена
-            refresh_token = RefreshToken(refresh_token)
-            access_token = refresh_token.access_token
-
-        except TokenError as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response({'access_token': str(access_token)})
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 
 class CustomGetToken(TokenObtainPairView):
@@ -27,7 +27,7 @@ class CustomGetToken(TokenObtainPairView):
         response = super().post(request, *args, **kwargs)
         if 'refresh' in response.data:
             response.set_cookie(
-                key='refresh_token',
+                key='refresh_token11',
                 value=response.data['refresh'],
                 httponly=True,
                 max_age=30 * 24 * 60 * 60,
