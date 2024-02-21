95a04ed32847ab723febfcbd85b3b8a47efe9a1c
justbegan <justbegan@mail.ru>
2024-02-21 16:03:54 +0900

    added registration

--- apps/jwt_auth/urls.py
+++ apps/jwt_auth/urls.py
@@ -1,5 +1,5 @@
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/register', include('djoser.urls')),
     # custom
     path('api/token/customRefresh', views.CustomTokenRefresh.as_view()),
     path('api/token/customGetToken', views.CustomGetToken.as_view())
