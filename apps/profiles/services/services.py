5972ac6728644e10b4e9caee07315edf03206d94
justbegan <justbegan@mail.ru>
2024-02-29 16:45:25 +0900

    fix env file

--- apps/profiles/services/services.py
+++ apps/profiles/services/services.py
@@ -1,14 +1,18 @@
 from requests import request
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def get_all_contest_from_fast_api() -> dict:
+    contest_url = os.environ.get('CONTEST_URL')
     response = request(
         method='GET',
         headers={
             "Content-Type": "application/json",
             "Accept": "*/*"
         },
-        url='http://127.0.0.1:8000/contes/get_contests'
+        url=f'{contest_url}/contes/get_contests'
     )
     result = {item['_id']: item['title'] for item in response.json()['data']}
     return result
