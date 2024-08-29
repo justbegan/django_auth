ac1dd31d81ab9518c24e736a140db02c36cf1bd6
justbegan <justbegan@mail.ru>
2024-08-29 17:10:23 +0900

    fix filter

--- apps/constructor/api/filter.py
+++ apps/constructor/api/filter.py
@@ -6,6 +6,8 @@ import json
 class Application_filter(django_filters.FilterSet):
     id = django_filters.CharFilter(method='filter_id')
     created_time = django_filters.CharFilter(method='filter_created_time')
+    municipal_district = django_filters.CharFilter(method='filter_municipal_district')
+    project_type = django_filters.CharFilter(method='filter_project_type')
     status = django_filters.CharFilter(method='filter_status')
     custom_data = django_filters.CharFilter(method='fiter_custom_data')
     order_by = django_filters.CharFilter(method='filter_order_by')
@@ -15,7 +17,6 @@ class Application_filter(django_filters.FilterSet):
         fields = ['id', 'created_time', 'status']
 
     def filter_id(self, queryset, name, value):
-        print(name)
         return queryset.filter(id=value)
 
     def filter_created_time(self, queryset, name, value):
@@ -24,7 +25,15 @@ class Application_filter(django_filters.FilterSet):
 
     def filter_status(self, queryset, name, value):
         status = value.split(',')
-        return queryset.filter(status__title__in=status)
+        return queryset.filter(status__id__in=status)
+
+    def filter_municipal_district(self, queryset, name, value):
+        district = value.split(',')
+        return queryset.filter(municipal_district__id__in=district)
+
+    def filter_project_type(self, queryset, name, value):
+        project_type = value.split(',')
+        return queryset.filter(project_type__id__in=project_type)
 
     def fiter_custom_data(self, queryset, name, value: dict):
         value = json.loads(value)
