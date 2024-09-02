20afe66b6880765caf7555230bfab4415c2e4f2c
justbegan <justbegan@mail.ru>
2024-09-02 09:15:27 +0900

    fix 02-09-24 morning fix

--- apps/constructor/api/views.py
+++ apps/constructor/api/views.py
@@ -27,13 +27,13 @@ class StandardResultsSetPagination(PageNumberPagination):
     def get_paginated_response(self, data):
         return Response({
             'main_table_fields': get_main_table_fields_by_section_method(self.request),
+            'win_lose': win_lose_calculation(self.request),
             'total_results': self.page.paginator.count,
             'total_pages': self.page.paginator.num_pages,
             'current_page': self.page.number,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data,
-            'win_lose': win_lose_calculation(self.request)
         })
 
 
