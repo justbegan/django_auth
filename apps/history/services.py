bfd90fd338616edc737f9075ae9b831791fee6ac
justbegan <justbegan@mail.ru>
2024-09-10 17:50:40 +0900

    history fix

--- apps/history/services.py
+++ apps/history/services.py
@@ -2,16 +2,42 @@ from rest_framework.views import Request, Response
 from deepdiff import DeepDiff
 from copy import deepcopy
 import logging
+from django.core.exceptions import ObjectDoesNotExist
+from dictdiffer import diff
 
 from .serializers import History_serializer
 from apps.constructor.api.services.crud import create
-from apps.constructor.models import Application
+from apps.constructor.models import Application, Schema
 
 
 logger = logging.getLogger('django')
 
 
+def get_field_description(field_name: str) -> str:
+    try:
+        # Получаем поле модели по его имени
+        field = Application._meta.get_field(field_name)
+        # Возвращаем verbose_name поля
+        return field.verbose_name
+    except ObjectDoesNotExist:
+        # Если поле не найдено, возвращаем None
+        return field_name
+
+
+def get_custom_field_description(schema: Schema, field_data: dict) -> str:
+    result = {}
+    fields = schema.get(id=1).properties
+    for k, v in field_data.items():
+        field = fields.get(k)
+        if field:
+            result[field['title']] = v
+        else:
+            result[k] = v
+    return result
+
+
 def get_histories_by_application_id(request: Request, id: int):
+    # schema = Schema.objects
     instance = Application.objects.get(id=id)
     history = instance.history.all()
     changes = []
@@ -19,13 +45,25 @@ def get_histories_by_application_id(request: Request, id: int):
         new_record = history[i - 1]
         old_record = history[i]
         delta = new_record.diff_against(old_record)
+
         for change in delta.changes:
             change_data = {
-                "field": change.field,
-                "new": change.new,
-                "old": change.old,
-                "автор": new_record.history_user.username if new_record.history_user else "Unknown"
+                "time": new_record.history_date,
+                "field": get_field_description(change.field),
+                "author": new_record.history_user.username if new_record.history_user else "Unknown"
             }
+            if change.field == 'custom_data':
+                # Сравниваем данные
+                differences = diff(change.old, change.new)
+
+                # Преобразуем результат в словарь
+                diff_dict = {
+                    change[0]: {change[1]: change[2]} for change in differences
+                }
+                change_data["test"] = diff_dict
+            else:
+                change_data["new"] = change.new
+                change_data["old"] = change.old
             changes.append(change_data)
     return Response(changes)
 
