ff6f5faf458f74a480503afdf549e7a69262acdf
justbegan <justbegan@mail.ru>
2024-09-17 15:13:40 +0900

    fix redis signals

--- apps/locations/signals.py
+++ apps/locations/signals.py
@@ -1,24 +1,41 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.core.cache import cache
+import logging
+
 from .models import (Locality, Settlement)
+
+
+logger = logging.getLogger('django')
 
 
 @receiver(post_save, sender=Locality)
 def locality_post_save(sender, instance, created, **kwargs):
-    cache.delete("all_locality")
+    try:
+        cache.delete("all_locality")
+    except Exception as e:
+        logger.exception(f"redis не найден {e}")
 
 
 @receiver(post_delete, sender=Locality)
 def locality_post_delete(sender, instance, **kwargs):
-    cache.delete("all_locality")
+    try:
+        cache.delete("all_locality")
+    except Exception as e:
+        logger.exception(f"redis не найден {e}")
 
 
 @receiver(post_save, sender=Settlement)
 def settlement_post_save(sender, instance, created, **kwargs):
-    cache.delete("all_settlements")
+    try:
+        cache.delete("all_settlements")
+    except Exception as e:
+        logger.exception(f"redis не найден {e}")
 
 
 @receiver(post_delete, sender=Settlement)
 def settlement_post_delete(sender, instance, **kwargs):
-    cache.delete("all_settlements")
+    try:
+        cache.delete("all_settlements")
+    except Exception as e:
+        logger.exception(f"redis не найден {e}")
