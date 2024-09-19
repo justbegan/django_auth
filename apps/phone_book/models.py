102e7fafc384cee1fe40de0a708a0e6983448c29
justbegan <justbegan@mail.ru>
2024-09-19 16:55:13 +0900

    fix phone_book

--- apps/phone_book/models.py
+++ apps/phone_book/models.py
@@ -4,9 +4,10 @@ from apps.profiles.models import Section
 
 class Phone_book(models.Model):
     fio = models.CharField("ФИО", max_length=100)
-    position = models.CharField("Должность", max_length=100)
+    position = models.TextField("Должность")
     description = models.TextField("Описание", blank=True, null=True)
     phone = models.CharField("Телефон", max_length=20)
+    ip_phone = models.CharField("Телефон", max_length=20)
     email = models.EmailField("Email", blank=True, null=True)
     section = models.ForeignKey(Section, verbose_name="Раздел", on_delete=models.PROTECT)
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
--- apps/phone_book/serializers.py
+++ apps/phone_book/serializers.py
@@ -13,4 +13,5 @@ class Phone_book_serializer_ff(serializers.Serializer):
     position = serializers.CharField()
     description = serializers.CharField()
     phone = serializers.CharField()
+    ip_phone = serializers.CharField()
     email = serializers.CharField()
