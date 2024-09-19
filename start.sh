c87305c203a979bf1c90842cc49118816c704353
justbegan <justbegan@mail.ru>
2024-09-19 11:21:17 +0900

    update start script

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 python manage.py migrate --noinput
 
+python manage.py sync_apps --noinput
+
 python manage.py collectstatic --noinput
 
 exec gunicorn main_app.wsgi:application --bind 0.0.0.0:8088 --workers 5
