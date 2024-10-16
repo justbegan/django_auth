791f02169dd139ef93af55bf3cdd08d043130c1b
justbegan <justbegan@mail.ru>
2024-10-16 09:35:10 +0900

    fix news

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from apps.profiles.models import Section
 
 
 class News(models.Model):
     title = models.TextField("Наименование")
     text = models.TextField("Текст")
     hide = models.BooleanField("Скрытый", default=False)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name='Секция')
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата обновления', auto_now=True)
 
--- apps/news/services.py
+++ apps/news/services.py
@@ -1,19 +1,25 @@
 from rest_framework.views import Request, Response
+from copy import deepcopy
 
 from .models import News
 from .serializers import News_serializer
 from services.crud import get_many, create, update, delete, get
+from apps.constructor.services.current import get_current_section
 
 
 def get_all_news(request: Request):
-    return Response(get_many(News, News_serializer, {"hide": False}, "-id"))
+    return Response(get_many(News, News_serializer, {"hide": False, "section": get_current_section(request)}, "-id"))
 
 
 def create_news(request: Request):
+    data = deepcopy(request.data)
+    data['section'] = get_current_section(request).id
     return Response(create(News_serializer, request.data))
 
 
 def update_news(request: Request, id: int):
+    data = deepcopy(request.data)
+    data['section'] = get_current_section(request).id
     return Response(update(News, News_serializer, request.data, {"id": id}))
 
 
